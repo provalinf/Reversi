f56421683af5a6c7605c2fd1e59b62d7770e0fc9
bonnal4u <co.merce@hotmail.fr>
2018-02-14 12:05:28 +0100

    Début algo coups possibles

--- src/Etat/EtatReversi.java
+++ src/Etat/EtatReversi.java
@@ -1,13 +1,19 @@
 package Etat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class EtatReversi extends Etat {
 	private Integer grille[][];
 	private static final int SIZE = 8;
+	private static final int VIDE = -1;
+	private static final int NOIR = 0;
+	private static final int BLANC = 1;
 
 	public EtatReversi() {
 		grille = new Integer[SIZE][SIZE]; //0 = noir, 1 = blanc, -1 = vide
+		/*for (Integer[] aGrille : grille) Arrays.fill(aGrille, -1);*/
 		etatInitial();
 	}
 
@@ -20,6 +26,10 @@ public class EtatReversi extends Etat {
 	}
 
 	private void etatInitial() {
+		/*grille[getSize()[0] / 2][getSize()[0] / 2] = 0;
+		grille[getSize()[0] / 2][getSize()[1] / 2] = 1;
+		grille[getSize()[1] / 2][getSize()[0] / 2] = 1;
+		grille[getSize()[1] / 2][getSize()[1] / 2] = 0;*/
 		for (int i = 0; i < grille.length; i++) {
 			for (int j = 0; j < grille[0].length; j++) {
 				if ((i == SIZE / 2-1 && j == SIZE / 2-1) || (i == SIZE / 2 && j == SIZE / 2 )) {
@@ -51,12 +61,30 @@ public class EtatReversi extends Etat {
 		return true;
 	}
 
+	public List<Integer> coupPossibles(int couleur) {
+		ArrayList<Integer> coups = new ArrayList<>();
+		int ligne = 0;
+		while (ligne < getSize()[0]) {
+			int colonne = 0;
+			while (colonne < getSize()[1]) {
+				if (getCase(ligne, colonne) == VIDE) {
+					if (colonne - 1 >= 0) {
+						if (getCase(ligne, colonne - 1) != VIDE) {
+
+						}
+					}
+				}
+			}
+		}
+		return coups;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < SIZE; i++) {
 			for (int j = 0; j < SIZE; j++) {
-				sb.append(grille[i][j]);
+				sb.append(grille[i][j]+"\t");
 			}
 			sb.append("\n");
 		}
--- src/joueur/JoueurReversi.java
+++ src/joueur/JoueurReversi.java
@@ -14,6 +14,5 @@ public class JoueurReversi extends Joueur {
 
 	public void successeurs(){
 		tourJoueur = tourJoueur+1%2; // permet de changer de joueur automatiquement
-
 	}
 }
